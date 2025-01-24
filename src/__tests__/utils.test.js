6b8c21d94b1a6fd13bc29331da33cc695a8e243c
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-01-24 10:22:59 +0300

    wrote my first test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,33 @@
 // Your tests here
+import {isPalindrome} from "../utils";
+
+describe ("isPalindrome", () =>{
+    it("should return true for a palidrome word", ()=>{
+        const word = "racecar";
+        const result = isPalindrome(word);
+        expect(result).toBe(true);
+    });
+    it("should return false for a non-palindrome word", () => {
+        const word = "car";
+        const result = isPalindrome(word);
+        expect(result).toBe(false);
+    });
+
+    it("should return true for a single-character string", () => {
+        const word = "a";
+        const result = isPalindrome(word);
+        expect(result).toBe(true);
+    });
+
+    it("should return true for an empty string", () => {
+        const word = "";
+        const result = isPalindrome(word);
+        expect(result).toBe(true);
+    });
+
+    it("should be case insensitive", () => {
+        const word = "RaceCar";
+        const result = isPalindrome(word);
+        expect(result).toBe(true);
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
 // Your code here
+export function isPalindrome(word){
+    const orgWord = word.toLowerCase();
+
+    const revWord = orgWord.split('').reverse().join('');
+
+    return orgWord === revWord;
+};
